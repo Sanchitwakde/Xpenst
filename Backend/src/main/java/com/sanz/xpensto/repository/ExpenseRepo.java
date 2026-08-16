12ea068c943ab3a7bda11282885f36ffb044544e
Sanchitwakde <wakdes70@gmail.com>
2026-08-16 18:38:40 +0530

    updated ExpenseRepo

--- Backend/src/main/java/com/sanz/xpensto/repository/ExpenseRepo.java
+++ Backend/src/main/java/com/sanz/xpensto/repository/ExpenseRepo.java
@@ -4,8 +4,16 @@ import com.sanz.xpensto.model.ExpenseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.time.LocalDate;
+
 @Repository
 public interface ExpenseRepo extends JpaRepository<ExpenseModel, Long> {
+
+    List<ExpenseModel> findByUserId(Long id);
+    List<ExpenseModel> findByUserIdOrderByDateDesc(Long userId);
+    List<ExpenseModel> findByUserIdAndCategoryIgnoreCase(Long userId, String category);
+    List<ExpenseModel> findByUserIdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
 }
 // extending jpa repo so it tells spring that entity class - ExpenseModel
 // and primary key is type - Long
